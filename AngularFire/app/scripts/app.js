934d0b0661256e870900e1c2a77d2dfd212e2342
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-02-08 11:15:50 +0900

    angular fire folder added

---
+++ AngularFire/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularFireApp
+ * @description
+ * # angularFireApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularFireApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ AngularFire/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFireApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularFireApp
+ */
+angular.module('angularFireApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
